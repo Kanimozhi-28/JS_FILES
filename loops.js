e6ec1fe7a990f3d180b4cb139071277d563e505c
kanimozhi <kanimozhi2877@gmail.com>
2025-11-03 19:18:23 +0530

    Initial commit

---
+++ loops.js
@@ -0,0 +1,21 @@
+
+        let i;
+        for(i=1;i<=5;i++){
+            console.log("*")
+
+        }
+
+        let a;
+        a=1
+        while(a<=10){
+            console.log("*")
+            i++
+        }
+
+        i=1
+        do{
+            console.log("*")
+            i++
+
+        }while(i<=6);
+
